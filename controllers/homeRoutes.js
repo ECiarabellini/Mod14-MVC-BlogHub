5a2f4d3a360404107557a4fdabab700e7c0b0a9a
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-03-06 00:06:48 -0800

    Display blog posts on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,48 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post} = require('../models');
 const withAuth = require('../utils/auth');
 
 // Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
+
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render('homepage', {
+//       users,
+//       // Pass the logged in flag to the template
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+//homepage
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-    });
+    const posts = dbPostData.map((post) =>
+      post.get({ plain: true })
+    );
+    res.render('homepage', { posts });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
+
 
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
